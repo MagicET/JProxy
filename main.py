34746d9d274336249045b6a926f1622b514aa867
Magicet <60283888+MagicET@users.noreply.github.com>
2025-08-28 01:36:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
-from typing import Optional
-
-from fastapi import FastAPI
+from openai import OpenAI
+from fastapi import FastAPI, Request, Header
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://janitorai.com"],
+    allow_credentials=True,
+    allow_methods=["POST"],
+    allow_headers=["authorization, content-type"],
+)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def openai_stream_generator(data, url, key):
+    client = OpenAI(
+        base_url = url,
+        api_key = key
+    )
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+    completion = client.chat.completions.create(
+        model=data.get("model"),
+        messages=data.get("messages"),
+        temperature=data.get("temperature"),
+        stream=data.get("stream")
+    )
+
+    for chunk in completion:
+        yield "data: " + chunk.json() + "\n\n"
+
+@app.post("/proxy")
+async def callApi(reqest: Request, url: str, Authorization: str = Header("None")):
+    data = await reqest.json()
+    
+    key = Authorization.replace("Bearer ", "")
+
+    return StreamingResponse(openai_stream_generator(data, url, key), media_type="text/event-stream")
