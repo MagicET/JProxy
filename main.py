17d864e92564ccf799668de13f7509a40fb48206
Magicet <60283888+MagicET@users.noreply.github.com>
2025-08-29 17:16:30 +0900

    Enabled exception catching

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from openai import OpenAI
-from fastapi import FastAPI, Request, Header
+from openai import OpenAI, OpenAIError
+from fastapi import FastAPI, Request, Header, HTTPException
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -13,26 +13,37 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-def openai_stream_generator(data, url, key):
-    client = OpenAI(
-        base_url = url,
-        api_key = key
-    )
-
-    completion = client.chat.completions.create(
-        model=data.get("model"),
-        messages=data.get("messages"),
-        temperature=data.get("temperature"),
-        stream=data.get("stream")
-    )
-
-    for chunk in completion:
-        yield "data: " + chunk.json() + "\n\n"
-
 @app.post("/proxy")
 async def callApi(reqest: Request, url: str, Authorization: str = Header("None")):
+    def openai_stream_caller(data, url, key):
+        try:
+            client = OpenAI(
+                base_url = url,
+                api_key = key
+            )
+            
+            completion = client.chat.completions.create(
+                model=data.get("model"),
+                messages=data.get("messages"),
+                temperature=data.get("temperature"),
+                stream=data.get("stream")
+            )
+            
+            return completion
+        except OpenAIError as e:
+            raise HTTPException(status_code=e.status_code, detail=str(e.body))
+    
+    def completion_generator(completion):
+        for chunk in completion:
+                yield "data: " + chunk.json() + "\n\n"
+
     data = await reqest.json()
 
     key = Authorization.replace("Bearer ", "")
 
-    return StreamingResponse(openai_stream_generator(data, url, key), media_type="text/event-stream")
+    try:
+        completion = openai_stream_caller(data, url, key)
+    except HTTPException as e:
+        raise e
+    else:
+        return StreamingResponse(completion_generator(completion), media_type="text/event-stream")
